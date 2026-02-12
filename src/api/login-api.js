814c86ad9e4232968735c51de5bc6a7c4014c4dd
ianapuhin <epoxapuhin@gmail.com>
2026-02-12 16:14:37 +0800

    modify login-api.js

--- src/api/login-api.js
+++ src/api/login-api.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
-const API_URL = "http://127.0.0.1:8000/api/v1/auth";
+const API_URL = "http://127.0.0.1:8000/api";
 
 // user login
 export const userLogin = async (username, password) => {
     try {
-        const res = await axios.post(`${API_URL}/login`, {
+        const res = await axios.post(`${API_URL}/auth/login`, {
             username: username,
             password: password
         });
