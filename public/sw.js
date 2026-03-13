3463c348d86994b2973a58cd0e8cd812cdf6015a
ianapuhin <epoxapuhin@gmail.com>
2026-03-13 22:44:02 +0800

    commented out the service worker

--- public/sw.js
+++ public/sw.js
@@ -1,19 +1,19 @@
-self.addEventListener("install", () => {
-  console.log("Service Worker Installed");
-  self.skipWaiting();
-});
+// self.addEventListener("install", () => {
+//   console.log("Service Worker Installed");
+//   self.skipWaiting();
+// });
 
-self.addEventListener("activate", () => {
-  console.log("Service Worker Activated");
-});
+// self.addEventListener("activate", () => {
+//   console.log("Service Worker Activated");
+// });
 
-self.addEventListener("fetch", (event) => {
-  const url = new URL(event.request.url);
+// self.addEventListener("fetch", (event) => {
+//   const url = new URL(event.request.url);
 
-  // Ignore external domains like S3
-  if (url.origin !== self.location.origin) {
-    return;
-  }
+//   // Ignore external domains like S3
+//   if (url.origin !== self.location.origin) {
+//     return;
+//   }
 
-  event.respondWith(fetch(event.request));
-});
+//   event.respondWith(fetch(event.request));
+// });
--- src/main.jsx
+++ src/main.jsx
@@ -6,21 +6,21 @@ import "leaflet/dist/leaflet.css";
 import App from "./App.jsx";
 
 /* Remove old service workers */
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.getRegistrations().then((registrations) => {
-    registrations.forEach((registration) => registration.unregister());
-  });
-}
+// if ("serviceWorker" in navigator) {
+//   navigator.serviceWorker.getRegistrations().then((registrations) => {
+//     registrations.forEach((registration) => registration.unregister());
+//   });
+// }
 
-/* Register new service worker */
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker
-      .register("/sw.js")
-      .then(() => console.log("Service Worker Registered"))
-      .catch((err) => console.log("SW registration failed", err));
-  });
-}
+// /* Register new service worker */
+// if ("serviceWorker" in navigator) {
+//   window.addEventListener("load", () => {
+//     navigator.serviceWorker
+//       .register("/sw.js")
+//       .then(() => console.log("Service Worker Registered"))
+//       .catch((err) => console.log("SW registration failed", err));
+//   });
+// }
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
